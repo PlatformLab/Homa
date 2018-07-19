9ba50e9e437394b6cd7cfa6565b37c5a9818ab8e
Collin Lee <cstlee@cs.stanford.edu>
2018-07-19 16:39:29 -0700

    Convert MacAddress for use in Homa.

--- src/MacAddress.cc
+++ src/MacAddress.cc
@@ -1,12 +1,12 @@
-/* Copyright (c) 2011-2016 Stanford University
+/* Copyright (c) 2011-2018, Stanford University
  *
- * Permission to use, copy, modify, and distribute this software for any
+ * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
  *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR(S) DISCLAIM ALL WARRANTIES
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL AUTHORS BE LIABLE FOR
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
  * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
  * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
@@ -15,11 +15,13 @@
 
 #include "MacAddress.h"
 
-namespace RAMCloud {
+#include "Util.h"
+
+namespace Homa {
 
 /**
  * Create a new address from 6 bytes.
- * \param raw
+ * @param raw
  *      The raw bytes.
  */
 MacAddress::MacAddress(const uint8_t raw[6])
@@ -29,22 +31,22 @@ MacAddress::MacAddress(const uint8_t raw[6])
 
 /**
  * Create a new address from a string representation.
- * \param macStr
+ * @param macStr
  *      A MAC address like "01:23:45:67:89:ab". Uppercase is also allowed.
- * \throw Exception
+ * @throw Exception
  *      The format of the given \a macStr is invalid. If this is thrown,
  *      the contents of the address will be left unmodified.
  */
 MacAddress::MacAddress(const char* macStr)
 {
     unsigned int bytes[6];
-    int r = sscanf(macStr, "%02x:%02x:%02x:%02x:%02x:%02x", // NOLINT
-                   &bytes[0], &bytes[1], &bytes[2],
-                   &bytes[3], &bytes[4], &bytes[5]);
+    int r = sscanf(macStr, "%02x:%02x:%02x:%02x:%02x:%02x",  // NOLINT
+                   &bytes[0], &bytes[1], &bytes[2], &bytes[3], &bytes[4],
+                   &bytes[5]);
     if (r != 6 || macStr[17] != '\0')
-        throw Exception(HERE, format("Bad MAC address: %s", macStr));
+        throw BadAddress(HERE, Util::format("Bad MAC address: %s", macStr));
     for (uint32_t i = 0; i < 6; ++i)
-        address[i] = downCast<uint8_t>(bytes[i]);
+        address[i] = Util::downCast<uint8_t>(bytes[i]);
 }
 
 MacAddress::MacAddress(const MacAddress& other)
@@ -53,45 +55,27 @@ MacAddress::MacAddress(const MacAddress& other)
     memcpy(address, other.address, 6);
 }
 
-/**
- * Generate a random MAC address.
- * Guaranteed to not be a multicast address and in the locally administered mac
- * address range, so you end up with 46 bits of randomness.
- */
-MacAddress::MacAddress(Random _)
-{
-    uint64_t r = generateRandom();
-    for (uint32_t i = 0; i < 6; ++i) {
-        address[i] = r & 0xFF;
-        r >>= 8;
-    }
-    // set locally administered mac address, unicast
-    address[0] &= 0xFC;
-    address[0] |= 0x02;
-}
-
-inline string
+inline std::string
 MacAddress::toString() const
 {
     char buf[18];
-    snprintf(buf, sizeof(buf), "%02x:%02x:%02x:%02x:%02x:%02x",
-             address[0], address[1], address[2],
-             address[3], address[4], address[5]);
+    snprintf(buf, sizeof(buf), "%02x:%02x:%02x:%02x:%02x:%02x", address[0],
+             address[1], address[2], address[3], address[4], address[5]);
     return buf;
 }
 
 /**
- * \return
+ * @return
  *      True if the MacAddress consists of all zero bytes, false if not.
  */
 bool
 MacAddress::isNull() const
 {
     if (address[0] == 0 && address[1] == 0 && address[2] == 0 &&
-            address[3] == 0 && address[4] == 0 && address[5] == 0)
+        address[3] == 0 && address[4] == 0 && address[5] == 0)
         return true;
     else
         return false;
 }
 
-} // namespace RAMCloud
+}  // namespace Homa
--- src/MacAddressTest.cc
+++ src/MacAddressTest.cc
@@ -1,54 +1,46 @@
-/* Copyright (c) 2011-2016 Stanford University
+/* Copyright (c) 2011-2018, Stanford University
  *
- * Permission to use, copy, modify, and distribute this software for any
+ * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
  *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR(S) DISCLAIM ALL WARRANTIES
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL AUTHORS BE LIABLE FOR
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
  * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
  * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include "TestUtil.h"
 #include "MacAddress.h"
 
-namespace RAMCloud {
+#include <gtest/gtest.h>
 
-TEST(MacAddressTest, constructorRaw) {
+namespace Homa {
+
+TEST(MacAddressTest, constructorRaw)
+{
     uint8_t raw[] = {0xde, 0xad, 0xbe, 0xef, 0x98, 0x76};
-    EXPECT_EQ("de:ad:be:ef:98:76",
-              MacAddress(raw).toString());
+    EXPECT_EQ("de:ad:be:ef:98:76", MacAddress(raw).toString());
 }
 
-TEST(MacAddressTest, constructorString) {
-    EXPECT_EQ("de:ad:be:ef:98:76",
-              MacAddress("de:ad:be:ef:98:76").toString());
+TEST(MacAddressTest, constructorString)
+{
+    EXPECT_EQ("de:ad:be:ef:98:76", MacAddress("de:ad:be:ef:98:76").toString());
 }
 
-TEST(MacAddressTest, constructorRandom) {
-    for (uint32_t i = 0; i < 100; ++i) {
-        MacAddress r1(MacAddress::RANDOM);
-        // make sure it's a locally administered unicast address
-        EXPECT_EQ(2, r1.address[0] & 0x03);
-        MacAddress r2(MacAddress::RANDOM);
-        EXPECT_NE(r1.toString(),
-                  r2.toString());
-    }
-}
-
-TEST(MacAddressTest, toString) {
+TEST(MacAddressTest, toString)
+{
     // tested sufficiently in constructor tests
 }
 
-TEST(MacAddressTest, isNull) {
-    uint8_t raw[] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
-    MacAddress r(MacAddress::RANDOM);
-    EXPECT_TRUE(MacAddress(raw).isNull());
-    EXPECT_FALSE(r.isNull());
+TEST(MacAddressTest, isNull)
+{
+    uint8_t rawNull[] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
+    uint8_t rawNonNull[] = {0xde, 0xad, 0xbe, 0xef, 0x98, 0x76};
+    EXPECT_TRUE(MacAddress(rawNull).isNull());
+    EXPECT_FALSE(MacAddress(rawNonNull).isNull());
 }
 
-}  // namespace RAMCloud
+}  // namespace Homa
