c15d0289cdd6e58aefdaf1ae9e2d53c3c17bac81
Collin Lee <cstlee@cs.stanford.edu>
2018-08-01 22:46:25 -0700

    Add Homa::Core::MessageContext
    The MessageContext stores a Message's metadata and provides access
    to a Message's data.

---
+++ src/MessageContext.cc
@@ -0,0 +1,147 @@
+/* Copyright (c) 2018, Stanford University
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include "MessageContext.h"
+
+namespace Homa {
+namespace Core {
+
+/**
+ * Construct a MessageContext.
+ *
+ * MessageContext objects are constructed with a refCount of 1.
+ *
+ * @param dataHeaderLength
+ *      Number of bytes at the beginning of every packet used to hold the Homa
+ *      protocol DataHeader. This should be the same for every packet in a given
+ *      message, but may be different for different messages since they may use
+ *      different versions of the protocol header.
+ * @param driver
+ *      Driver from which packets were/will be allocated and to which they
+ *      should be returned when this message is no longer needed.
+ * @param messagePool
+ *      Pool from which this MessageContext was allocated and to which it should
+ *      be returned on destruction.
+ *
+ * @sa MessageContext::release()
+ */
+MessageContext::MessageContext(uint16_t dataHeaderLength, Driver* driver,
+                               MessagePool* messagePool)
+    : address(nullptr)
+    , messageLength(0)
+    , PACKET_DATA_LENGTH(driver->getMaxPayloadSize() - dataHeaderLength)
+    , DATA_HEADER_LENGTH(dataHeaderLength)
+    , messagePool(messagePool)
+    , driver(driver)
+    , refCount(1)
+    , numPackets(0)
+    , occupied()
+    , packets()
+{}
+
+/**
+ * Destruct a MessageContext. Will release all contained Packet objects.
+ */
+MessageContext::~MessageContext()
+{
+    // if the message is complete, we can release all the packets all at once.
+    if (messageLength <= numPackets * PACKET_DATA_LENGTH) {
+        driver->releasePackets(packets, numPackets);
+    } else {  // otherwise, we need to find which packets need to be released.
+        for (uint16_t i = 0; i < MAX_MESSAGE_PACKETS && numPackets > 0; ++i) {
+            if (occupied.test(i)) {
+                driver->releasePackets(packets + i, 1);
+                --numPackets;
+            }
+        }
+    }
+}
+
+/**
+ * Return the Packet with the given index.
+ *
+ * @param index
+ *      A Packet's index in the array of packets that form the message.
+ *      "packet index = "packet message offset" / PACKET_DATA_LENGTH
+ * @return
+ *      Pointer to a Packet at the given index if it exists; nullptr otherwise.
+ */
+Driver::Packet*
+MessageContext::getPacket(uint16_t index)
+{
+    if (occupied.test(index)) {
+        return packets[index];
+    }
+    return nullptr;
+}
+
+/**
+ * Store the given packet as the Packet of the given index if one does not
+ * already exist.
+ *
+ * @param index
+ *      The Packet's index in the array of packets that form the message.
+ *      "packet index = "packet message offset" / PACKET_DATA_LENGTH
+ * @param packet
+ *      The packet pointer that should be stored.
+ * @return
+ *      True if the packet was stored; false if a packet already exists (the new
+ *      packet is not stored).
+ */
+bool
+MessageContext::setPacket(uint16_t index, Driver::Packet* packet)
+{
+    if (occupied.test(index)) {
+        return false;
+    }
+    packets[index] = packet;
+    occupied.set(index);
+    numPackets++;
+    return true;
+}
+
+/**
+ * Used by a module to indicate that it would like to retain access to this
+ * MessageContext. When the module no longer needs access, it should call
+ * MessageContext::release().
+ *
+ * @sa MessageContext::release()
+ */
+void
+MessageContext::retain()
+{
+    // if the refCount is 0, something bad happend.
+    assert(refCount > 0);
+    refCount++;
+}
+
+/**
+ * Used by a module to indicate it no longer needs access to this MessageContext
+ * object. Normally called after previously calling MessageContext::retain().
+ *
+ * @sa MessageContext::retain()
+ */
+void
+MessageContext::release()
+{
+    refCount--;
+    if (refCount.load() < 1) {
+        messagePool->destroy(this);
+        // can't do anything after this, the object is destroyed
+    }
+}
+
+}  // namespace Core
+}  // namespace Homa
---
+++ src/MessageContext.h
@@ -0,0 +1,100 @@
+/* Copyright (c) 2018, Stanford University
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef HOMA_CORE_MESSAGECONTEXT_H
+#define HOMA_CORE_MESSAGECONTEXT_H
+
+#include "Driver.h"
+#include "ObjectPool.h"
+
+#include <atomic>
+#include <bitset>
+
+namespace Homa {
+namespace Core {
+
+// forward declaration
+class MessageContext;
+
+/// A memory allocator for MessageConetxt objects.
+typedef ObjectPool<MessageContext> MessagePool;
+
+/**
+ * Holds the contents and metadata from a Transport::Message.
+ *
+ * The lifetime of instances of this class are controlled with reference counts.
+ *
+ * This class is not thread-safe but should only be modified by one part of
+ * the transport a time.
+ */
+class MessageContext {
+  public:
+    /// Define the maximum number of packets that a message can hold.
+    static const uint16_t MAX_MESSAGE_PACKETS = 1024;
+
+    explicit MessageContext(uint16_t dataHeaderLength, Driver* driver,
+                            MessagePool* messagePool);
+    ~MessageContext();
+
+    Driver::Packet* getPacket(uint16_t index);
+    bool setPacket(uint16_t index, Driver::Packet* packet);
+    void retain();
+    void release();
+
+    /// Contains the source address for a recevied message and and the
+    /// destination for an sent message.
+    Driver::Address* address;
+
+    /// Total length of the message.
+    uint32_t messageLength;
+
+    /// Number of bytes of data in each full packet.
+    const uint16_t PACKET_DATA_LENGTH;
+
+    /// Number of bytes used by the Homa protocol header in each packet.
+    const uint16_t DATA_HEADER_LENGTH;
+
+  private:
+    /// ObjectPool from which this MessageContext was allocated and to which it
+    /// should be returned on destruction.
+    MessagePool* messagePool;
+
+    /// Driver from which packets were allocated and to which they should be
+    /// returned when this message is no longer needed.
+    Driver* driver;
+
+    /// Number of times this context has been retained (see retain()).
+    /// Constructing a instances of MessageCountext set the refCount to 1.
+    /// When the refCount reaches 0, the instance will be destroyed.
+    std::atomic<int> refCount;
+
+    /// Number of packets contained in this message.
+    uint16_t numPackets;
+
+    /// Bit array representing which entires in the _packets_ array are set.
+    /// Used to avoid having to zero out the entire _packets_ array.
+    std::bitset<MAX_MESSAGE_PACKETS> occupied;
+
+    /// Collection of packets that form this message.
+    Driver::Packet* packets[MAX_MESSAGE_PACKETS];
+
+    MessageContext(const MessageContext&) = delete;
+    MessageContext& operator=(const MessageContext&) = delete;
+};
+
+}  // namespace Core
+}  // namespace Homa
+
+#endif  // HOMA_CORE_MESSAGECONTEXT_H
